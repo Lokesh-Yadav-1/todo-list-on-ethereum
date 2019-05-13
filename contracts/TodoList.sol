10b6a687164a638f5db4c3fe5623a3f5ca62b34b
lokesh <lokeshyadav1291@gmail.com>
2019-05-14 00:45:26 +0530

    list create

--- contracts/TodoList.sol
+++ contracts/TodoList.sol
@@ -1,5 +1,24 @@
 pragma solidity ^0.5.0;
 
 contract TodoList {
-    uint public taskCoun = 0;
+    uint public taskCount = 0;
+
+    struct task{
+        uint id;
+        string detail;
+        bool completed;
+
+    }
+
+    mapping(uint => task) public tasks;
+
+    constructor() public {
+        createTask("complete MTP");
+    }
+
+    function createTask(string memory _content) public{
+        taskCount ++;
+        tasks[taskCount] = task(taskCount, _content, false);
+    }
+
 }
